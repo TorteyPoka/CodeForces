e9f9f6d007e3d4176ae30b0cbdcf14deb76cccad
TorteyPoka <torteypoka@gmail.com>
2024-01-27 20:22:29 +0600

    ok?

---
+++ Contenst/Educational Codeforces Round 161 (Rated for Div. 2)/D_Berserk_Monsters.cpp
@@ -0,0 +1,31 @@
+/* Solved by Sakhawat Hossain Mahin, CSE-28th, SMUCT */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        int b[n];
+        vector<pair<int, int>>v;
+        vector<int>ans;
+
+        for(int i = 0; i<n; i++)
+            cin>>a[i];
+        for(int i = 0; i<n; i++){
+            cin>>b[i];
+        }
+        
+
+    }
+    return 0;
+}
