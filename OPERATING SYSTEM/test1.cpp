a6e35076f80358d12345ef01c63b870525bb9b41
TorteyPoka <torteypoka@gmail.com>
2024-03-28 10:05:30 +0600

    prac_os

--- OPERATING SYSTEM/test1.cpp
+++ OPERATING SYSTEM/test1.cpp
@@ -1,11 +1,13 @@
+/* Solved by Sakhawat Hossain Mahin, CSE-28th, SMUCT */
 #include <bits/stdc++.h>
+#define ll long long
+#define int long long
+
 using namespace std;
 
-void firstFit(int blockSize[], int m, int processSize[], int n)
+void firstFit(int *blockSize, int m, int *processSize, int n)
 {
-    int allocation[n];
-    memset(allocation, -1, sizeof(allocation));
-
+    vector<int> allocation(n + 1, -1);
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -19,24 +21,30 @@ void firstFit(int blockSize[], int m, int processSize[], int n)
         }
     }
 
-    cout << "\nProcess No.\tProcess Size\tBlock no.\n";
+    cout << "Process No.\tProcess Size\tBlock No." << endl;
     for (int i = 0; i < n; i++)
     {
-        cout << " " << i + 1 << "\t\t" << processSize[i] << "\t\t";
+        cout << i + 1 << "\t\t" << processSize[i] << "\t\t";
         if (allocation[i] != -1)
-            cout << allocation[i] + 1;
+            cout << allocation[i];
         else
             cout << "Not Allocated";
         cout << endl;
     }
 }
 
-int main()
+signed main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
     int blockSize[] = {100, 500, 200, 300, 600};
     int processSize[] = {212, 417, 112, 426};
     int m = sizeof(blockSize) / sizeof(blockSize[0]);
     int n = sizeof(processSize) / sizeof(processSize[0]);
+
     firstFit(blockSize, m, processSize, n);
+
     return 0;
 }
